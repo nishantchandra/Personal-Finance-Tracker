7208dbdef310e31207ed61ecaaac57b6984d64c1
Nishant Chandra <nishantchandra1@gmail.com>
2014-04-23 23:43:34 -0700

    set up repo for this app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinanceTracker::Application.config.secret_key_base = '61ef676b5907079c2f5ebcae779d31f959766acab0972eb85fbe562fe754282b0d8167f6a457fe95f1f3ce5dbc669c9f7300b2fa9c6a08b7da8906db0a531ee3'
