97006a3c34fb1ecd75cd0b7636b54df704fde138
Nishant Chandra <nishantchandra1@gmail.com>
2014-04-28 23:46:48 -0700

    change names of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 FinanceTracker::Application.routes.draw do
 
-  resources :yahoo, only: [:home, :update, :destroy, :new, :create]
+  resources :yahoo, only: [:home, :destroy, :new, :create]
 
+  get "new", to: "yahoo#new"
+  get "update", to: "yahoo#update"
   root :to => 'yahoo#home'
-
-   post "update", to: "yahoo#update"
+   # post "update", to: "yahoo#update"
 
 end
